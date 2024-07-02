6df6e6b54770d17e83d1a34a89b636bf0b46214f
pritamcn <you@example.com>
2024-07-02 17:21:48 +0530

    prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express,{Express,Request,Response} from "express";
+import { PORT } from "./secrets";
 
 const app=express();
 app.get("/",(req:Request,res:Response)=>{
     res.send("hello world");
 })
-app.listen(3000,()=>{
+app.listen(PORT, ()=>{
     console.log("server is running on port 3000");
 })
