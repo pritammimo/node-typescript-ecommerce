a682c0aca55633d090c7ea53f1c169665484f427
pritamcn <you@example.com>
2024-07-02 14:47:12 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ app.get("/",(req:Request,res:Response)=>{
     res.send("hello world");
 })
 app.listen(3000,()=>{
-    console.log("server is running on port 3000");
+    console.log("server is running on 3000");
 })
